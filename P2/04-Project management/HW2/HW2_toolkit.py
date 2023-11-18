a4df3fc94073e35358858e33f3f0dbe351a58c66
KeipS <kj.pj.sj@gmail.com>
2023-11-18 21:11:07 +0330

    Management

--- P2/04-Project management/HW2/HW2_toolkit.py
+++ P2/04-Project management/HW2/HW2_toolkit.py
@@ -2,7 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.metrics import r2_score
 
 class Forcasting_Formulas:
     """
@@ -39,7 +43,7 @@ class Forcasting_Formulas:
             new_data = new_data._append(predict_data, ignore_index=True)
             updated_data = updated_data._append(predict_data, ignore_index=True)
         new_data.index = range(1, 1 + len(new_data))
-        return new_data, updated_data
+        return new_data.iloc[-number_of_prediction:, 1], updated_data
 
     def weighted_moving_average(self, number_of_data: int, weights: list, number_of_prediction: int) -> [pd.DataFrame,
                                                                                                          pd.DataFrame]:
@@ -63,13 +67,90 @@ class Forcasting_Formulas:
             new_data = new_data._append(predict_data, ignore_index=True)
             updated_data = updated_data._append(predict_data, ignore_index=True)
         new_data.index = range(1, 1 + len(new_data))
-        return new_data, updated_data
+        return new_data.iloc[-number_of_prediction:, 1], updated_data
 
-    def exponential_smoothing(self):
-        pass
+    @staticmethod
+    def _exponential_smoothing_myself(data: pd.DataFrame, alpha: float) -> float:
+        """
+        Calculates the exponential smoothing of the data and predicts the future value.
+        """
+        initial_prediction = data.iloc[9:, 1].mean()
+        predict = 0
+        new_data = data.iloc[:, :].copy()
+        for i in range(len(data)):
+            predict = new_data.iloc[i, 1] * alpha + (1 - alpha) * initial_prediction
+            initial_prediction = predict
+        return predict
 
-    def trend_adjusted_exponential_smoothing(self):
-        pass
+    def serious_exponential_smoothing_myself(self, all_data: list, alpha: float) -> [pd.DataFrame, pd.DataFrame]:
+        """
+        Calculates the exponential smoothing of the data and predicts the future values.
+        """
+        predicted_data = pd.DataFrame({0: [], 1: []})
+        total_data = self.data.copy()
+        for new_data in all_data:
+            the_new_data = self._exponential_smoothing_myself(new_data, alpha)
+            predicted_data = predicted_data._append(pd.DataFrame({0: [new_data.iloc[-1, 0] + 1], 1: [the_new_data]}),
+                                                    ignore_index=True)
+        total_data = total_data._append(predicted_data, ignore_index=True)
+        predicted_data.index = range(1, 1 + len(predicted_data))
+        total_data.index = range(1, 1 + len(total_data))
+        return predicted_data.iloc[-len(all_data):, 1], total_data
+
+    def exponential_smoothing(self, number_of_prediction: int, alpha: float, beta: float = None):
+        """
+        Generates a prediction using exponential smoothing.
+
+        Args:
+            number_of_prediction (int): The number of steps to forecast.
+            alpha (float): The smoothing level.
+            beta (float, optional): The smoothing trend. Only for Trend_adjusted method. Defaults to None.
+
+        Returns:
+            pandas.DataFrame: A DataFrame containing the predicted data.
+        """
+        predicted_data = pd.DataFrame({0: [], 1: []})
+        data = self.data.copy()
+        data.iloc[:, 0] = pd.to_datetime(data.iloc[:, 0])
+        data.set_index(0, inplace=True)
+        data2 = self.data.copy()
+        model = ExponentialSmoothing(self.data[1], trend="add", initialization_method="estimated", seasonal_periods=12,
+                                     use_boxcox=True).fit(smoothing_level=alpha, smoothing_trend=beta)
+        prediction = model.forecast(steps=number_of_prediction)
+        for i in range(len(prediction)):
+            predicted_data = predicted_data._append(
+                pd.DataFrame({0: [data2.iloc[-1, 0] + 1 + i], 1: [prediction.iloc[i]]}),
+                ignore_index=True)
+        data2 = data2._append(predicted_data, ignore_index=True)
+        data2.index = range(1, 1 + len(data2))
+        return predicted_data.iloc[-number_of_prediction:, 1], data2
+
+    def score(self, method: list, true_val, predict_val) -> float:
+        """
+        Calculate the score using different methods.
+
+        Parameters:
+            method (list): A list of strings indicating the methods to be used for calculating the score. Valid options are "MAE", "MSE", R2, and "MAD".
+            true_val: The true values.
+            predict_val: The predicted values.
+
+        Returns:
+            float: The calculated score.
+
+        Raises:
+            None
+        """
+        error = {}
+        if "MAE" in method:
+            error["MAE"] = mean_absolute_error(true_val, predict_val)
+        if "MSE" in method:
+            error["MSE"] = mean_squared_error(true_val, predict_val, squared=True)
+        if "MAD" in method:
+            error["MAD"] = mean_absolute_percentage_error(true_val, predict_val)
+        if "R2" in method:
+            error["R2"] = r2_score(true_val, predict_val)
+
+        return error
 
     @staticmethod
     def plotting(data: pd.DataFrame, data_name: str) -> None:
